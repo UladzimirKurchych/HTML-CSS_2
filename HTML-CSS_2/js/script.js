3017fe94c1feae48d35590c75a015b5cc9f2fe95
uladziku <kurchich.v@gmail.com>
2022-11-26 18:04:29 +0100

    Update script.js

--- HTML-CSS_2/js/script.js
+++ HTML-CSS_2/js/script.js
@@ -1,1 +1,15 @@
 
+$(document).ready(function(){
+	$(function(){
+
+		$('.header-btn').on('click', function(){
+			$('.menu').addClass('active');
+		});
+		
+		$('.close-btn').on('click', function(){
+			$('.menu').removeClass('active');
+		});
+
+		}); 
+});
+
